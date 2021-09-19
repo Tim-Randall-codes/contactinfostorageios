e90cdd10f62fe1a2f6025146d8d76334fc6a064f
Tim Randall <trtimrandall@gmail.com>
2021-09-19 16:37:54 +1000

    Initial Commit

---
+++ contactinfostorage/contactinfostorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactinfostorageApp.swift
+//  contactinfostorage
+//
+//  Created by Tim Randall on 19/9/21.
+//
+
+import SwiftUI
+
+@main
+struct contactinfostorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
